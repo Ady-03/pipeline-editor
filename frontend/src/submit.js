6a508997a5a2cf5160c45d5de1b8194465394da6
Ady-03 <kain.adi0304@gmail.com>
2026-05-30 13:16:31 +0530

    added Frontend api call

--- frontend/src/submit.js
+++ frontend/src/submit.js
@@ -1,5 +1,7 @@
 export async function submitPipeline(nodes, edges) {
-  const response = await fetch("http://localhost:8000/pipelines/parse", {
+  const BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+
+  const response = await fetch(`${BASE_URL}/pipelines/parse`, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ nodes, edges }),
